820dec6f22fa2acd4649caac52faa239ce800d8a
github2136 <github2136@163.com>
2017-11-21 14:45:29 +0800

    提交图片条码二维码识别

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Zxing_Android3.3.0
 [![](https://jitpack.io/v/github2136/Zxing_Android3.3.0.svg)](https://jitpack.io/#github2136/Zxing_Android3.3.0)  
-本功能基于zxing3.3.0开发，除了zxing的core、android-core，使用少量代码实现二维码扫描功能，扫描界面为竖屏，扫描框默认200dp*200dp。  
+本功能基于zxing3.3.0开发，除了zxing的core、android-core，使用少量代码实现二维码扫描功能，扫描界面为竖屏，扫描框默认200dp*200dp。
+如需要做图片识别需要添加
+**compile 'com.github.github2136:Android-utils:ba2f38b4f2'**
+**compile 'com.google.code.gson:gson:2.7'**
 ```
 Intent intent = new Intent(this, ZxingActivity.class);
 //设置扫描框尺寸，宽高必须成对设置否则无效，如果同时设置DP和PX以PX为准
-intent.putExtra(ZxingActivity.KEY_SCAN_WIDTH_DP,300);
-intent.putExtra(ZxingActivity.KEY_SCAN_HEIGHT_DP,300);
-intent.putExtra(ZxingActivity.KEY_SCAN_WIDTH_PX,300);
-intent.putExtra(ZxingActivity.KEY_SCAN_HEIGHT_PX,300);
-intent.putExtra(ZxingActivity.KEY_SCAN_HEIGHT_SCALE, 0.0);//边框位置控制0.0-1.0
-intent.putExtra(ZxingActivity.KEY_SCAN_COLOR, ResourcesCompat.getColor(getResources(),R.color.colorPrimary,null));//边框颜色设置，默认为Color.BLUE
+intent.putExtra(ZxingActivity.AEG_SCAN_WIDTH_DP,300);
+intent.putExtra(ZxingActivity.AEG_SCAN_HEIGHT_DP,300);
+intent.putExtra(ZxingActivity.AEG_SCAN_WIDTH_PX,300);
+intent.putExtra(ZxingActivity.AEG_SCAN_HEIGHT_PX,300);
+intent.putExtra(ZxingActivity.AEG_SCAN_HEIGHT_SCALE, 0.0);//边框位置控制0.0-1.0
+intent.putExtra(ZxingActivity.AEG_SCAN_COLOR, ResourcesCompat.getColor(getResources(),R.color.colorPrimary,null));//边框颜色设置，默认为Color.BLUE
 startActivityForResult(intent, 1);
 
 ```
--- android/src/main/java/com/google/zxing/client/android/ZxingActivity.java
+++ android/src/main/java/com/google/zxing/client/android/ZxingActivity.java
@@ -1,6 +1,7 @@
 package com.google.zxing.client.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -10,23 +11,28 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.github2136.util.BitmapUtil;
+import com.github2136.util.FileUtil;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.ReaderException;
 import com.google.zxing.Result;
+import com.google.zxing.common.GlobalHistogramBinarizer;
 import com.google.zxing.common.HybridBinarizer;
 
 import java.io.IOException;
@@ -39,16 +45,19 @@ import java.util.Map;
 import java.util.Set;
 
 public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Callback, Camera.PreviewCallback {
-    public static final String KEY_RESULT = "RESULT";
-    public static final String KEY_SCAN_WIDTH_PX = "SCAN_WIDTH_PX";
-    public static final String KEY_SCAN_HEIGHT_PX = "SCAN_HEIGHT_PX";
-    public static final String KEY_SCAN_WIDTH_DP = "SCAN_WIDTH_DP";
-    public static final String KEY_SCAN_HEIGHT_DP = "SCAN_HEIGHT_DP";
-    public static final String KEY_SCAN_HEIGHT_SCALE = "SCAN_HEIGHT_SCALE";
-    public static final String KEY_SCAN_COLOR = "SCAN_COLOR";
+    public static final String ARG_RESULT = "RESULT";
+    public static final String ARG_SCAN_PIC = "SCAN_PIC";
+    public static final String ARG_SCAN_WIDTH_PX = "SCAN_WIDTH_PX";
+    public static final String ARG_SCAN_HEIGHT_PX = "SCAN_HEIGHT_PX";
+    public static final String ARG_SCAN_WIDTH_DP = "SCAN_WIDTH_DP";
+    public static final String ARG_SCAN_HEIGHT_DP = "SCAN_HEIGHT_DP";
+    public static final String ARG_SCAN_HEIGHT_SCALE = "SCAN_HEIGHT_SCALE";
+    public static final String ARG_SCAN_COLOR = "SCAN_COLOR";
+    private static final int REQUEST_SELECT_IMG = 249;
+    private Context mContext;
     private SurfaceView surfaceView;
     private ViewfinderView vView;
-    private ImageButton ibFlash;
+    private ImageButton ibFlash, ibScanning;
     private ImageView iv;//结果图
     private Camera camera;
     private Camera.Size bestSize;
@@ -58,34 +67,45 @@ public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Ca
     private BeepManager beepManager;
     private Map<DecodeHintType, Object> hints;//解码类型
     private boolean mRotation = false;//结果图旋转
+    private Set<String> mMimeType = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zxing);
+        mMimeType.add("image/jpeg");
+        mMimeType.add("image/png");
+        mMimeType.add("image/gif");
+        mContext = this;
         surfaceView = (SurfaceView) findViewById(R.id.preview_view);
         vView = (ViewfinderView) findViewById(R.id.vv_view);
         ibFlash = (ImageButton) findViewById(R.id.ib_flash);
+        ibScanning = (ImageButton) findViewById(R.id.ib_scanning);
         ibFlash.setOnClickListener(mOnClickListener);
+        ibScanning.setOnClickListener(mOnClickListener);
         iv = (ImageView) findViewById(R.id.iv);
 
 //        initHints();
-        if (getIntent().hasExtra(KEY_SCAN_WIDTH_PX) && getIntent().hasExtra(KEY_SCAN_HEIGHT_PX)) {
-            int width = getIntent().getIntExtra(KEY_SCAN_WIDTH_PX, 600);
-            int height = getIntent().getIntExtra(KEY_SCAN_HEIGHT_PX, 600);
+        if (getIntent().hasExtra(ARG_SCAN_WIDTH_PX) && getIntent().hasExtra(ARG_SCAN_HEIGHT_PX)) {
+            int width = getIntent().getIntExtra(ARG_SCAN_WIDTH_PX, 600);
+            int height = getIntent().getIntExtra(ARG_SCAN_HEIGHT_PX, 600);
             vView.setScanWidthPx(width);
             vView.setScanHeightPx(height);
-        } else if (getIntent().hasExtra(KEY_SCAN_WIDTH_DP) && getIntent().hasExtra(KEY_SCAN_HEIGHT_DP)) {
-            int width = getIntent().getIntExtra(KEY_SCAN_WIDTH_DP, 200);
-            int height = getIntent().getIntExtra(KEY_SCAN_HEIGHT_DP, 200);
+        } else if (getIntent().hasExtra(ARG_SCAN_WIDTH_DP) && getIntent().hasExtra(ARG_SCAN_HEIGHT_DP)) {
+            int width = getIntent().getIntExtra(ARG_SCAN_WIDTH_DP, 200);
+            int height = getIntent().getIntExtra(ARG_SCAN_HEIGHT_DP, 200);
             vView.setScanWidthDp(width);
             vView.setScanHeightDp(height);
         }
-        if (getIntent().hasExtra(KEY_SCAN_COLOR)) {
-            vView.setScanColor(getIntent().getIntExtra(KEY_SCAN_COLOR, Color.BLUE));
+        if (getIntent().hasExtra(ARG_SCAN_PIC) && getIntent().getBooleanExtra(ARG_SCAN_PIC, false)) {
+            ibScanning.setVisibility(View.VISIBLE);
         }
-        if (getIntent().hasExtra(KEY_SCAN_HEIGHT_SCALE)) {
-            vView.setHeightScale(getIntent().getDoubleExtra(KEY_SCAN_HEIGHT_SCALE, 0.5));
+
+        if (getIntent().hasExtra(ARG_SCAN_COLOR)) {
+            vView.setScanColor(getIntent().getIntExtra(ARG_SCAN_COLOR, Color.BLUE));
+        }
+        if (getIntent().hasExtra(ARG_SCAN_HEIGHT_SCALE)) {
+            vView.setHeightScale(getIntent().getDoubleExtra(ARG_SCAN_HEIGHT_SCALE, 0.5));
         }
         vView.reset();
 
@@ -132,37 +152,42 @@ public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Ca
     private View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (camera != null) {
-                Camera.Parameters parameters = camera.getParameters();
-                if (parameters != null) {
-                    if (parameters.getFlashMode().equals(Camera.Parameters.FLASH_MODE_TORCH)) {
-                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                        ibFlash.setImageResource(R.drawable.zxing_ic_flash_off);
-                    } else {
-                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                        ibFlash.setImageResource(R.drawable.zxing_ic_flash_on);
+            int i = v.getId();
+            if (i == R.id.ib_flash) {
+                if (camera != null) {
+                    Camera.Parameters parameters = camera.getParameters();
+                    if (parameters != null) {
+                        if (parameters.getFlashMode().equals(Camera.Parameters.FLASH_MODE_TORCH)) {
+                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                            ibFlash.setImageResource(R.drawable.zxing_ic_flash_off);
+                        } else {
+                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                            ibFlash.setImageResource(R.drawable.zxing_ic_flash_on);
+                        }
+                        camera.setParameters(parameters);
                     }
-                    camera.setParameters(parameters);
                 }
+            } else if (i == R.id.ib_scanning) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, REQUEST_SELECT_IMG);
             }
         }
     };
 
     @Override
     protected void onResume() {
-        Log.e("tag","onResume");
         super.onResume();
         if (hasSurface) {
-            Log.e("tag","hasSurface");
             SurfaceHolder surfaceHolder = surfaceView.getHolder();
             openCamera(surfaceHolder);
+            startPreview(surfaceHolder);
         }
-        surfaceView.setVisibility(View.VISIBLE);
     }
 
     @Override
     protected void onPause() {
-        Log.e("tag","onpause");
         super.onPause();
         ibFlash.setImageResource(R.drawable.zxing_ic_flash_off);
         if (camera != null) {
@@ -181,7 +206,6 @@ public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Ca
             autoFocusManager.stop();
             autoFocusManager = null;
         }
-        surfaceView.setVisibility(View.GONE);
     }
 
     //预览图与屏幕比例
@@ -190,63 +214,63 @@ public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Ca
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
         Camera.Size size = camera.getParameters().getPreviewSize(); //获取预览大小
-//        int w;  //宽度
-//        int h;
-//        byte[] b;
-//        if (mRotation) {
-//            w = size.height;  //宽度
-//            h = size.width;
-//            b = roate90YUVdata(data, size);
-//        } else {
-//            w = size.width;  //宽度
-//            h = size.height;
-//            b = data;
-//        }
-//        if (sourceScale == 0) {
-//            sourceScale = w / (float) getResources().getDisplayMetrics().widthPixels;
-//        }
-//        //获取预览图片，预览图片为横向显示
-//      /*  final YuvImage image = new YuvImage(data, ImageFormat.NV21, scanningWidth, scanningHeight, null);
-//        ByteArrayOutputStream os = new ByteArrayOutputStream(data.length);
-//        image.compressToJpeg(new Rect(0, 0, scanningWidth, scanningHeight), 100, os);
-//        byte[] tmp = os.toByteArray();
-//        Bitmap bmp = BitmapFactory.decodeByteArray(tmp, 0, tmp.length);*/
-//
-//
-//        //裁剪框图片
-//        PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(b, w, h,
-//                (int) ((w - vView.getScanWidthPx() * sourceScale) / 2),
-//                (int) ((h - vView.getScanHeightPx() * sourceScale) * vView.getHeightScale()),
-//                (int) (vView.getScanWidthPx() * sourceScale),
-//                (int) (vView.getScanHeightPx() * sourceScale), false);
-//
-//
-//        if (iv.getVisibility() == View.VISIBLE) {
-//            //显示裁剪框图片
-//            int[] pixels = source.renderThumbnail();
-//            int width = source.getThumbnailWidth();
-//            int height = source.getThumbnailHeight();
-//            Bitmap bitmap1 = Bitmap.createBitmap(pixels, 0, width, width, height, Bitmap.Config.ARGB_8888);
-//            iv.setImageBitmap(bitmap1);
-//        }
-//        Result rawResult = null;
-//
-//        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-//
-//        try {
-//            rawResult = multiFormatReader.decodeWithState(bitmap);
-//        } catch (ReaderException re) {
-//        } finally {
-//            multiFormatReader.reset();
-//        }
-//
-//        if (rawResult != null) {
-//            beepManager.playBeepSoundAndVibrate();
-//            Intent intent = new Intent();
-//            intent.putExtra(KEY_RESULT, rawResult.toString());
-//            setResult(RESULT_OK, intent);
-//            finish();
-//        }
+        int w;  //宽度
+        int h;
+        byte[] b;
+        if (mRotation) {
+            w = size.height;  //宽度
+            h = size.width;
+            b = roate90YUVdata(data, size);
+        } else {
+            w = size.width;  //宽度
+            h = size.height;
+            b = data;
+        }
+        if (sourceScale == 0) {
+            sourceScale = w / (float) getResources().getDisplayMetrics().widthPixels;
+        }
+        //获取预览图片，预览图片为横向显示
+      /*  final YuvImage image = new YuvImage(data, ImageFormat.NV21, scanningWidth, scanningHeight, null);
+        ByteArrayOutputStream os = new ByteArrayOutputStream(data.length);
+        image.compressToJpeg(new Rect(0, 0, scanningWidth, scanningHeight), 100, os);
+        byte[] tmp = os.toByteArray();
+        Bitmap bmp = BitmapFactory.decodeByteArray(tmp, 0, tmp.length);*/
+
+
+        //裁剪框图片
+        PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(b, w, h,
+                (int) ((w - vView.getScanWidthPx() * sourceScale) / 2),
+                (int) ((h - vView.getScanHeightPx() * sourceScale) * vView.getHeightScale()),
+                (int) (vView.getScanWidthPx() * sourceScale),
+                (int) (vView.getScanHeightPx() * sourceScale), false);
+
+
+        if (iv.getVisibility() == View.VISIBLE) {
+            //显示裁剪框图片
+            int[] pixels = source.renderThumbnail();
+            int width = source.getThumbnailWidth();
+            int height = source.getThumbnailHeight();
+            Bitmap bitmap1 = Bitmap.createBitmap(pixels, 0, width, width, height, Bitmap.Config.ARGB_8888);
+            iv.setImageBitmap(bitmap1);
+        }
+        Result rawResult = null;
+
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            rawResult = multiFormatReader.decodeWithState(bitmap);
+        } catch (ReaderException re) {
+        } finally {
+            multiFormatReader.reset();
+        }
+
+        if (rawResult != null) {
+            beepManager.playBeepSoundAndVibrate();
+            Intent intent = new Intent();
+            intent.putExtra(ARG_RESULT, rawResult.toString());
+            setResult(RESULT_OK, intent);
+            finish();
+        }
     }
 
     public static byte[] roate90YUVdata(byte[] yuvData, Camera.Size size) {
@@ -271,6 +295,23 @@ public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Ca
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        startPreview(holder);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (beepManager != null) {
+            beepManager.close();
+        }
+        super.onDestroy();
+    }
+
+    private void startPreview(SurfaceHolder holder) {
         Camera.Parameters parameters = camera.getParameters();
         int[] fps = new int[]{0, 0};
         for (int[] i : parameters.getSupportedPreviewFpsRange()) {
@@ -291,19 +332,6 @@ public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Ca
         setCameraDisplayOrientation(this, getDefaultCameraId(), camera);
         camera.startPreview();
         autoFocusManager = new AutoFocusManager(this, camera);
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        hasSurface = false;
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (beepManager != null) {
-            beepManager.close();
-        }
-        super.onDestroy();
     }
 
     private void openCamera(SurfaceHolder holder) {
@@ -489,5 +517,68 @@ public class ZxingActivity extends AppCompatActivity implements SurfaceHolder.Ca
             }
         }
         return defaultId;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_SELECT_IMG:
+                    String p = FileUtil.getFileAbsolutePath(this, data.getData());
+                    String suffix = FileUtil.getSuffix(p);
+                    //获取文件后缀
+                    MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+                    if (mMimeType.contains(mimeTypeMap.getMimeTypeFromExtension(suffix))) {
+                        BitmapUtil
+                                .getInstance(p)
+                                .limit(1080)
+                                .get(new BitmapUtil.BitmapGetCallBack() {
+                                         @Override
+                                         public void callback(Bitmap bitmap) {
+                                             scanningImage(bitmap);
+                                         }
+                                     }
+                                );
+
+                    } else {
+                        Toast.makeText(this, "非图片类型文件(jpg、png、gif)", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void scanningImage(Bitmap scanBitmap) {
+        Result result = null;
+        RGBLuminanceSource source = null;
+        try {
+            int width = scanBitmap.getWidth();
+            int height = scanBitmap.getHeight();
+            int[] pixels = new int[width * height];
+            scanBitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+            source = new RGBLuminanceSource(width, height, pixels);
+            result = multiFormatReader.decode(new BinaryBitmap(new HybridBinarizer(source)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (source != null) {
+                try {
+                    result = multiFormatReader.decode(new BinaryBitmap(new GlobalHistogramBinarizer(source)));
+                } catch (Throwable e2) {
+                    e2.printStackTrace();
+                }
+            }
+
+        } finally {
+            multiFormatReader.reset();
+            if (result != null) {
+                beepManager.playBeepSoundAndVibrate();
+                Intent intent = new Intent();
+                intent.putExtra(ARG_RESULT, result.toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            } else {
+                Toast.makeText(mContext, "未扫描到数据", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
