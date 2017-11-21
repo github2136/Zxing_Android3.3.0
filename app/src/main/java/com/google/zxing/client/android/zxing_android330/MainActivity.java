71e8ae46ccda87f6e51278da78c731c0e8863e1f
github2136 <github2136@163.com>
2017-11-21 14:45:29 +0800

    提交图片条码二维码识别

--- app/src/main/java/com/google/zxing/client/android/zxing_android330/MainActivity.java
+++ app/src/main/java/com/google/zxing/client/android/zxing_android330/MainActivity.java
@@ -1,14 +1,12 @@
 package com.google.zxing.client.android.zxing_android330;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.google.zxing.client.android.ZxingActivity;
@@ -26,9 +24,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ZxingActivity.class);
-                intent.putExtra(ZxingActivity.KEY_SCAN_WIDTH_DP, 200);
-                intent.putExtra(ZxingActivity.KEY_SCAN_HEIGHT_DP, 200);
-                intent.putExtra(ZxingActivity.KEY_SCAN_COLOR, ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null));
+                intent.putExtra(ZxingActivity.ARG_SCAN_WIDTH_DP, 200);
+                intent.putExtra(ZxingActivity.ARG_SCAN_HEIGHT_DP, 200);
+                intent.putExtra(ZxingActivity.ARG_SCAN_PIC, true);
+                intent.putExtra(ZxingActivity.ARG_SCAN_COLOR, ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null));
                 startActivityForResult(intent, 1);
             }
         });
@@ -37,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            tv1.setText(data.getStringExtra(ZxingActivity.KEY_RESULT));
+            tv1.setText(data.getStringExtra(ZxingActivity.ARG_RESULT));
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
